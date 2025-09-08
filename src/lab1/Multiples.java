c7a8b5af33d72e580f40aad0a57dcce53b0af2ff
kynmiiso <155723165+kynmiiso@users.noreply.github.com>
2025-09-08 18:58:49 -0400

    completed task 4 multiples and passed tests

---
+++ src/lab1/Multiples.java
@@ -0,0 +1,25 @@
+package lab1;
+
+public class Multiples {
+    public static void main(String[] args) {
+        int count = multiples();
+        System.out.println(count);
+    }
+
+    public static int multiples(int n, int a, int b) {
+        int i = 1;
+        int count = 0;
+        while (i < n) {
+            if (i % a == 0 || i % b == 0) {
+                count++;
+                System.out.println(i);
+            }
+            i++;
+        }
+        return count;
+    }
+
+    public static int multiples() {
+        return multiples(1000, 3, 5);
+    }
+}
